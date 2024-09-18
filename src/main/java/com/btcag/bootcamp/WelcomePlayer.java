fa1d462be078de2ccf406c15482082816cec7cff
Carstens <jannis.carstens@btc-ag.com>
2024-09-18 16:52:49 +0200

    finished task

--- src/main/java/com/btcag/bootcamp/WelcomePlayer.java
+++ src/main/java/com/btcag/bootcamp/WelcomePlayer.java
@@ -1,4 +1,12 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class WelcomePlayer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte geben Sie ihren Gamertag ein: ");
+        String username = scanner.nextLine();
+        System.out.println("Willkommen " + username + "!");
+    }
 }
