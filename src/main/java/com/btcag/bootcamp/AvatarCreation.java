09af16666601362b4f05daa3db78fa76be88eef6
Carstens <jannis.carstens@btc-ag.com>
2024-09-19 11:59:57 +0200

    ascii avatars added

--- src/main/java/com/btcag/bootcamp/AvatarCreation.java
+++ src/main/java/com/btcag/bootcamp/AvatarCreation.java
@@ -1,7 +1,20 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class AvatarCreation {
     public static void main(String[] args) {
+        String Robo1 = " \\_/\n" +
+                "(* *)\n" +
+                " )#(";
+
+        System.out.println(Robo1);
+
+        String Robo2 = " __ \n" +
+                "|@@|\n" +
+                "\\--/";
+
+        System.out.println(Robo2);
 
     }
 }
