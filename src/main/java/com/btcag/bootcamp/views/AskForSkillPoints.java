6b8172ef97cecfd51028b9ae6105e2de4cc75808
Carstens <jannis.carstens@btc-ag.com>
2024-11-28 13:31:58 +0100

    AskForSkillPoints

---
+++ src/main/java/com/btcag/bootcamp/views/AskForSkillPoints.java
@@ -0,0 +1,68 @@
+package com.btcag.bootcamp.views;
+
+import com.btcag.bootcamp.models.Robot;
+
+import java.util.Scanner;
+
+public class AskForSkillPoints {
+    public static void display(Robot robot){
+        int skillpoints = 10;
+        Scanner readIn = new Scanner(System.in);
+        while (skillpoints > 0){
+            System.out.println("Du hast noch " + skillpoints + " skillpunkte.");
+            System.out.println("""
+                Bitte wähle erst aus welche fähigkeit du leveln möchtest,
+                anschließen wie viele Skillpunkte du verteilen willst.
+                
+                [1] - Movement
+                [2] - Damage
+                [3] - Health
+                [4] - Range
+                «-----------------------------------------------»
+                """);
+        int choice = readIn.nextInt();
+        int temp = displayAmount();
+
+
+            if (choice == 1) {
+                robot.setMovementRange(displayAmount());
+                skillpoints-=temp;
+            } else if (choice == 2) {
+                robot.setAttackDamage(displayAmount());
+                skillpoints-=temp;
+            } else if (choice == 3) {
+                robot.setHp(displayAmount());
+                skillpoints-=temp;
+            } else if (choice == 4) {
+                robot.setAttackRange(displayAmount());
+                skillpoints-=temp;
+            } else {
+                System.out.println("Ungültige auswahl");
+            }
+
+        }
+    }
+
+     public static int displayAmount(){
+        Scanner readIn = new Scanner(System.in);
+        System.out.println("""
+                «-----------------------------------------------»
+                Wie viele Skillpunkte möchtest du vergeben,
+                du hast 10 zum verteilen. [1-10]
+                «-----------------------------------------------»
+                """);
+
+        int temp = readIn.nextInt();
+        return  temp;
+    }
+
+    public static void displayStats(Robot robot){
+        System.out.println("«-----------------------------------------------»\n"  +
+                "Du hast folgende Skillpunkte verteilt: "+
+                "\nHealthpoints: " + robot.getHp()+
+                "\nDamage: " + robot.getAttackDamage() +
+                "\nRange: " + robot.getAttackRange() +
+                "\nBewegunsrate: " + robot.getMovementRange()+
+                "\n«-----------------------------------------------»");
+    }
+}
